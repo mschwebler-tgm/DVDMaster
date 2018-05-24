3b47943cb488a781cf7ba53bb5a381715ffe5a6e
mschwebler-tgm <matthias.schwebler1998@gmail.com>
2018-05-24 14:34:22 +0200

    finished design prototype

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,12 +4,13 @@ window.Vue = require('vue');
 
 import VueRouter from 'vue-router';
 import router from './router';
-
+import Transitions from 'vue2-transitions'
 import theMovieDb from './themoviedb';
 
 Vue.component('movie-cards', require('./components/MovieCards.vue'));
 
 Vue.use(VueRouter);
+Vue.use(Transitions);
 
 const app = new Vue({
     el: '#app',
--- resources/assets/js/components/AddMovie.vue
+++ resources/assets/js/components/AddMovie.vue
@@ -4,19 +4,46 @@
 
         </div>
         <div class="container hide-on-small-only z-depth-3">
-            <div class="movie-backdrop" :style="{'background-image': 'url(' + $root.tmdbImagePath + 'original' + movie.backdrop_path + ')'}">
-                <div class="row">
+            <div class="movie-backdrop" :style="{'background-image': movie.backdrop_path ? 'url(' + $root.tmdbImagePath + 'original' + movie.backdrop_path + ')' : ''}">
+                <div class="row" style="height: 420px;">
                     <i class="material-icons clear-movie tooltipped" data-position="right" data-tooltip="Clear" @click="clearMovie">clear</i>
                     <div class="col m6">
-                        <div class="input-field">
-                            <input id="title" type="text" class="form-control white-text" name="name" @blur="searchMovie" v-model="movie.title">
-                            <label for="title">Title</label>
+                        <div class="row" style="padding: 15px;">
+                            <div class="col s8">
+                                <div class="input-field">
+                                    <input id="title" type="text" class="form-control white-text" name="name"
+                                           v-model="movie.title" @blur="searchMovie" @keydown.enter="searchMovie" @keydown.tab="searchMovie" @keydown="registerChange">
+                                    <label for="title">Title</label>
+                                </div>
+                            </div>
+                            <div class="col s12 ratings">
+                                <div class="star-ratings">
+                                <star-rating :increment="0.5" style="z-index: 1000;" class="custom-rating" inactive-color="white"
+                                             active-color="#FFD700" v-model="movie.custom_rating" :show-rating="false"
+                                             :border-width="0" :star-size="40"></star-rating>
+                                <star-rating :increment="0.01" :rating="movie.vote_average/2" :show-rating="false" class="tmdb-rating"
+                                             active-color="#aaaaaa" inactive-color="white" :border-width="0"
+                                             v-if="!movie.custom_rating" :star-size="40"></star-rating>
+                                </div>
+                                <fade-transition :duration="200">
+                                    <i class="material-icons white-text pointer tooltipped" data-position="right"
+                                       :data-tooltip="movie.vote_count + ' users rated on themoviedb.org'"
+                                       v-show="movie.vote_count && !movie.custom_rating">info</i>
+                                </fade-transition>
+                            </div>
+                        </div>
+                        <div class="input-field col m6 movie-overview">
+                            <textarea id="overview" class="materialize-textarea grey-text" v-model="movie.overview"></textarea>
+                            <label for="overview">Overview</label>
                         </div>
                     </div>
-                    <div class="col m6 center">
+                    <div class="col m6 poster-container">
                         <img :src="movie.poster_path ? ($root.tmdbImagePath + 'w185' + movie.poster_path)
                                                    : ('https://dentallabor-gruettner.de/wp-content/uploads/2017/05/placeholder.gif')"
                              class="movie-poster background-center">
+                        <p v-if="movie.release_date" class="grey-text" style="font-size: 14px;">
+                            {{ movie.release_date.substring(0, 4) }}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -27,7 +54,7 @@
                         <div class="collapsible-body">
                             <div class="row">
                                 <div class="col s12 suggestions" style="background-color: black;">
-                                <!-- swiper -->
+                                    <!-- swiper -->
                                     <swiper :options="swiperOption" v-if="readyForSuggestions">
                                         <swiper-slide v-for="suggestion in suggestions" :key="suggestion.id"
                                         v-if="suggestion.poster_path">
@@ -48,31 +75,13 @@
                     </li>
                 </ul>
             </div>
-
-            <!--<div class="row">-->
-                <!--<div class="col s12 z-depth-5 stage">-->
-                    <!--<div style="position: relative; max-height: 500px; overflow: hidden;">-->
-                        <!--<img :src="movie.backdrop_path ? ($root.tmdbImagePath + 'original' + movie.backdrop_path) :-->
-                                                     <!--('https://dentallabor-gruettner.de/wp-content/uploads/2017/05/placeholder.gif')"-->
-                             <!--class="featured-cover background-center">-->
-                        <!--<div style="position: absolute; top: 0; left: 0; display: flex; align-items: flex-end; width: 100%; height: 100%">-->
-                            <!--<div style="background-color: rgba(0,0,0,0.33); padding: 35px; width: 100%">-->
-                                <!--<div class="input-field">-->
-                                    <!--<input id="title" type="text"-->
-                                           <!--class="form-control white-text"-->
-                                           <!--name="name" @blur="searchMovie" v-model="movie.title">-->
-                                    <!--<label for="title" class="white-text">Title</label>-->
-                                <!--</div>-->
-                                <!--&lt;!&ndash;<h1 class="white-text" style="margin-top: 0;">{{ movie.title }}</h1>&ndash;&gt;-->
-                                <!--<p class="tagline">{{ movie.tagline }}</p>-->
-                            <!--</div>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-
-                <!--<div class="col s12" style="background-color: white;">-->
-                <!--</div>-->
-            <!--</div>-->
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <a class="waves-effect waves-light btn right">Save</a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -80,6 +89,7 @@
 <script>
     import '../../../../node_modules/swiper/dist/css/swiper.css';
     import {swiper, swiperSlide} from 'vue-awesome-swiper';
+    import StarRating from 'vue-star-rating';
 
     export default {
         name: "AddMovie",
@@ -93,7 +103,9 @@
                     spaceBetween: 0,
                 },
                 readyForSuggestions: true,
-                collapsibleInstances: []
+                collapsibleInstances: [],
+                lastSearchTerm: '',
+                searchTermChanged: false
             }
         },
         mounted() {
@@ -107,13 +119,21 @@
         methods: {
             searchMovie() {
                 let title = $('#title').val();
+                if (title === this.lastSearchTerm || !this.searchTermChanged) { return }
                 this.$root.theMovieDb.search.getMovie({query: title}, res => {
                     res = JSON.parse(res);
                     if (res.results && res.results.length > 0) {
                         this.suggestions = res.results;
                         this.movie = res.results.shift();
+                        this.lastSearchTerm = title;
+                        this.searchTermChanged = false;
 
                         $('#suggestions li:not(.active) #suggestionsTrigger').trigger('click');
+                        $('.movie-overview label').addClass('active');
+
+                        setTimeout(() => {
+                            M.textareaAutoResize($('#overview'));
+                        }, 100);
                     }
                 }, err => {
                     console.log(err);
@@ -121,15 +141,28 @@
             },
             selectMovie(movie) {
                 this.movie = movie;
+                setTimeout(() => {
+                    M.textareaAutoResize($('#overview'));
+                }, 100);
             },
             clearMovie() {
                 this.movie = {};
                 this.suggestions = [];
+            },
+            registerChange(event) {
+                console.log(event);
+                if (!this.searchTermChanged) {
+                    this.searchTermChanged = ['Enter', 'Tab', 'Control', 'Alt', 'Shift', 'Capslock', 'Escape',
+                        'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
+                        'AudioVolumeMute', 'AudioVolumeDown', 'AudioVolumeUp', 'End', 'PageDown', 'PageUp',
+                        'Home', 'Insert', 'NumLock'].indexOf(event.key) === -1;
+                }
             }
         },
         components: {
             swiper,
-            swiperSlide
+            swiperSlide,
+            StarRating
         }
     }
 </script>
@@ -139,6 +172,7 @@
         width: 185px;
         height: 278px;
         border: 3px groove #adbfbf;
+        margin-top: 15px;
     }
 
     #title {
@@ -266,6 +300,74 @@
         padding: 15px;
         color: white;
         font-size: 34px;
+    }
+
+    .tmdb-rating {
+        position: absolute;
+        top: 0;
+    }
+
+    .star-ratings:hover .custom-rating {
+        display: flex;
+    }
+
+    .star-ratings:hover .tmdb-rating {
+        display: none;
+    }
+
+    .custom-rating {
+        display: none;
+    }
+
+    .ratings {
+        position: relative;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .ratings i {
+        padding-left: 10px;
+    }
+
+    .movie-overview {
+        position: absolute;
+        bottom: 15px;
+        left: 15px;
+        max-height: 170px;
+        overflow: scroll;
+    }
+
+    .movie-overview::-webkit-scrollbar {
+        background-color: rgba(0,0,0,0);
+        width: 8px;
+        height: 8px;
+    }
+    .movie-overview::-webkit-scrollbar-button {
+        display: none;
+    }
+    .movie-overview::-webkit-scrollbar-track {
+        display: none;
+    }
+    .movie-overview::-webkit-scrollbar-track-piece {
+    }
+    .movie-overview::-webkit-scrollbar-thumb {
+        background-color: rgba(125,125,125,0.5);
+        border-radius: 10px;
+    }
+    .movie-overview::-webkit-scrollbar-corner {
+        display: none;
+    }
+    .movie-overview::-webkit-resizer {
+        display: none;
+    }
+
+    .poster-container {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
     }
 
 </style>
