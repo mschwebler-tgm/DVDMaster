41de8084e674a51162a1bfbfcefd61d65b7a34e3
mschwebler-tgm <matthias.schwebler1998@gmail.com>
2018-08-01 17:30:43 +0200

    improve search bar: clear input

--- resources/assets/js/components/SearchBar.vue
+++ resources/assets/js/components/SearchBar.vue
@@ -2,7 +2,7 @@
     <div class="input-field right" style="width: 400px; background-color: #CC444B;">
         <input id="search" type="search" style="height: 64px !important" v-model="query" @keyup.enter="search">
         <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-        <i class="material-icons">close</i>
+        <i class="material-icons" @click="reset">close</i>
     </div>
 </template>
 
@@ -16,6 +16,10 @@
         methods: {
             search() {
                 this.$store.dispatch('MOVIES_ACTION_SEARCH', this.query);
+            },
+            reset() {
+                this.query = '';
+                this.$store.dispatch('MOVIES_ACTION_GET_ALL');
             }
         }
     }
