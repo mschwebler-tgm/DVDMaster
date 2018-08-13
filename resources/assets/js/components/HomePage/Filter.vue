06bbb954b416503d11aaa0402b6a5ecc061a1f1d
mschwebler-tgm <matthias.schwebler1998@gmail.com>
2018-08-13 17:56:59 +0200

    Redesign: add boolean filters

--- resources/assets/js/components/HomePage/Filter.vue
+++ resources/assets/js/components/HomePage/Filter.vue
@@ -1,14 +1,40 @@
 <template>
     <transition name="filters">
         <md-content class="filters md-elevation-2" v-show="show">
-            <div class="content">
-                <div class="tag-filters">
-                    <div class="flex flex-align-center">
-                        <md-icon>bookmark</md-icon>&nbsp;&nbsp;<genres-input classes="custom-tag filter-tag"></genres-input> <span class="md-caption">Genres</span>
+            <div class="content-wrapper">
+                <div class="content">
+                    <div class="tag-filters">
+                        <div class="flex flex-align-center">
+                            <md-icon>bookmark</md-icon>&nbsp;&nbsp;<genres-input classes="custom-tag filter-tag"></genres-input> <span class="md-caption">Genres</span>
+                        </div>
+                        <div class="flex flex-align-center">
+                            <md-icon>person</md-icon>&nbsp;&nbsp;<actors-input classes="custom-tag filter-tag"></actors-input> <span class="md-caption">Actors</span>
+                        </div>
                     </div>
-                    <div class="flex flex-align-center">
-                        <md-icon>person</md-icon>&nbsp;&nbsp;<actors-input classes="custom-tag filter-tag"></actors-input> <span class="md-caption">Actors</span>
-                    </div>
+                </div>
+                <div class="content">
+                    <table>
+                        <tr>
+                            <td>
+                                <span class="icon-filters"><md-icon>import_export</md-icon> Borrowed</span>
+                            </td>
+                            <td>
+                                <span class="icon-filters"><md-icon>album</md-icon> Blue Ray</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span class="icon-filters"><md-icon>event</md-icon> True story</span>
+                            </td>
+                            <td>
+                                <span class="icon-filters"><md-icon>book</md-icon> Based on book</span>
+                            </td>
+                        </tr>
+                    </table>
+
+
+
+
                 </div>
                 <div class="close" @click="$parent.toggleFilters()">
                     <md-icon>expand_less</md-icon>
@@ -21,6 +47,11 @@
 <script>
     export default {
         props: ['show'],
+        data() {
+            return {
+                boolFilters: []
+            }
+        }
     }
 </script>
 
@@ -32,16 +63,28 @@
 
     .close {
         margin-left: auto;
-        padding-left: 10px;
+        padding: 10px;
         cursor: pointer;
     }
 
+    .content-wrapper {
+        display: flex;
+        height: 100%;
+    }
     .content {
         padding: 15px 15px 15px 30px;
         display: flex;
         align-items: center;
-        justify-content: space-around;
         height: 100%;
+    }
+
+    .content:first-child {
+        flex: 2;
+    }
+
+    .content:nth-child(2) {
+        flex: 1;
+        flex-wrap: wrap;
     }
 
     .tag-filters {
@@ -58,6 +101,10 @@
         -ms-transition: all 1s;
         -o-transition: all 1s;
         transition: all 1s;
+    }
+
+    .icon-filters {
+        padding: 5px;
     }
 
     /* Animations */
