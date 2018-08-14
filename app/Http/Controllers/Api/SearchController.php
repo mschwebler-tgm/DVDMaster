f71d716c43db8349cca917010b7d9179cec4a904
mschwebler-tgm <matthias.schwebler1998@gmail.com>
2018-08-14 13:47:17 +0200

    Redesign: add bool search backend

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Movie;
+use App\User;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+    private $isFirstQuery = true;
+
     public function movies(Request $request)
     {
         $movies = Movie::with('rentedBy', 'actors', 'genres', 'pendingRental')
@@ -14,15 +17,43 @@ class SearchController extends Controller
 
         $movies->where('title', 'like', "%{$request->get('title', '')}%");
         if ($request->has('genres')) {
-            $movies->whereHas('genres', function ($query) use ($request) {
+            $this->addQuery($movies, 'whereHas', 'genres', function ($query) use ($request) {
                 $query->whereIn('name', $request->get('genres', []));
             });
         }
         if ($request->has('actors')) {
-            $movies->whereHas('actors', function ($query) use ($request) {
+            $this->addQuery($movies, 'whereHas', 'actors', function ($query) use ($request) {
                 $query->whereIn('name', $request->get('actors', []));
             });
         }
+        if ($boolFilters = $request->get('bool')) {
+            $this->applyBoolfilters($movies, $boolFilters);
+        }
         return $movies->paginate();
+    }
+
+    private function applyBoolfilters(&$movies, $boolFilters)
+    {
+        foreach ($boolFilters as $boolFilter) {
+            if ($boolFilter === 'borrowed') {
+                $this->addQuery($movies, 'whereHas', 'pendingRental');
+            } else {
+                $this->addQuery($movies, 'where', $boolFilter, 1);
+            }
+        }
+    }
+
+    private function addQuery(&$movies, $method, $firstArg, $secondArg = null)
+    {
+        $methodPendants = [
+            'where' => 'orWhere',
+            'whereHas' => 'orWhereHas'
+        ];
+
+        if (!$this->isFirstQuery){
+            $method = $methodPendants[$method];
+        }
+        $movies->$method($firstArg, $secondArg);
+        $this->isFirstQuery = false;
     }
 }
