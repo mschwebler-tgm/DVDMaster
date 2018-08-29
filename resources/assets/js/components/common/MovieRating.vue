3743efed47c4c693973f4a455e27cdd9d7e7dd3f
mschwebler-tgm <matthias.schwebler1998@gmail.com>
2018-08-29 10:37:54 +0200

    make starrating readonly for non admins

--- resources/assets/js/components/common/MovieRating.vue
+++ resources/assets/js/components/common/MovieRating.vue
@@ -3,10 +3,10 @@
         <star-rating :increment="0.5" style="z-index: 1000;" class="custom-rating" inactive-color="white"
                      active-color="var(--md-theme-default-accent, #FFD700)" v-model="movie.custom_rating" :show-rating="false"
                      :border-width="0" :star-size="starSize || 40" @click="$emit('newCustomRating', movie.custom_rating)"
-                     glow-color="rgba(0,0,0,0)" :style="showCustomRating ? 'display: flex' : ''"></star-rating>
+                     glow-color="rgba(0,0,0,0)" :style="showCustomRating ? 'display: flex' : ''" :readOnly="!$root.isAdmin"></star-rating>
         <star-rating :increment="0.01" :rating="movie.vote_average/2" :show-rating="false" class="tmdb-rating"
                      active-color="#aaaaaa" inactive-color="#efefef" :border-width="0"
-                     v-if="!movie.custom_rating" :star-size="starSize || 40" glow-color="rgba(0,0,0,0)"></star-rating>
+                     v-if="!movie.custom_rating" :star-size="starSize || 40" glow-color="rgba(0,0,0,0)" :readOnly="!$root.isAdmin"></star-rating>
     </div>
 </template>
 
