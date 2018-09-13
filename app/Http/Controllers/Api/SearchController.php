2b264a63db3d6256eee83ea2b740473fb9aabb76
mschwebler-tgm <matthias.schwebler1998@gmail.com>
2018-09-13 12:25:33 +0200

    refactor: unify movies and series

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -4,47 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Movie;
 use App\Service\Facades\ContentTransformer;
-use App\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     private $isFirstQuery = true;
 
-    public function movies(Request $request)
+    public function search(Builder $builder, Request $request, $titleField)
     {
-        $movies = Movie::with('actors', 'genres', 'pendingRental.user');
-
-        $movies->where('title', 'like', "%{$request->get('title', '')}%");
+        $builder->where($titleField, 'like', "%{$request->get('title', '')}%");
         if ($request->has('genres')) {
-            $this->addQuery($movies, 'whereHas', 'genres', function ($query) use ($request) {
+            $this->addQuery($builder, 'whereHas', 'genres', function ($query) use ($request) {
                 $query->whereIn('name', $request->get('genres', []));
             });
         }
         if ($request->has('actors')) {
-            $this->addQuery($movies, 'whereHas', 'actors', function ($query) use ($request) {
+            $this->addQuery($builder, 'whereHas', 'actors', function ($query) use ($request) {
                 $query->whereIn('name', $request->get('actors', []));
             });
         }
         if ($boolFilters = $request->get('bool')) {
-            $this->applyBoolfilters($movies, $boolFilters);
+            $this->applyBoolfilters($builder, $boolFilters);
         }
+
+        return $builder;
+    }
+
+    public function movies(Request $request)
+    {
+        $movies = Movie::with('actors', 'genres', 'pendingRental.user');
+        $movies = $this->search($movies, $request, 'title');
         $movies = $movies->orderBy('title', 'asc')->paginate();
         return ContentTransformer::transformContentPaginaton($movies, 'movies');
     }
 
-    private function applyBoolfilters(&$movies, $boolFilters)
+    private function applyBoolfilters(&$content, $boolFilters)
     {
         foreach ($boolFilters as $boolFilter) {
             if ($boolFilter === 'borrowed') {
-                $this->addQuery($movies, 'whereHas', 'pendingRental');
+                $this->addQuery($content, 'whereHas', 'pendingRental');
             } else {
-                $this->addQuery($movies, 'where', $boolFilter, 1);
+                $this->addQuery($content, 'where', $boolFilter, 1);
             }
         }
     }
 
-    private function addQuery(&$movies, $method, $firstArg, $secondArg = null)
+    private function addQuery(&$content, $method, $firstArg, $secondArg = null)
     {
         $methodPendants = [
             'where' => 'orWhere',
@@ -54,7 +60,7 @@ class SearchController extends Controller
         if (!$this->isFirstQuery){
             $method = $methodPendants[$method];
         }
-        $movies->$method($firstArg, $secondArg);
+        $content->$method($firstArg, $secondArg);
         $this->isFirstQuery = false;
     }
 }
